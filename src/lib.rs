460bb18b6ceec5810ef68c45ba714158e0993f21
igavriil <jason@transifex.com>
2020-05-05 21:05:08 +0300

    Unbounded Myers' approximate string matching algorithm
    Impements the extended block-based algorithm for unbounded
    query size. This version does not implement Ukkonen's band,
    so the complete relocatable D.P. Matrix is calculated.
    
    The block size is 64 since the word size is 64 bit.
    This version does a byte-based allignment and not a char-based.

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,69 @@ pub fn myers_64(query: &str, text: &str) -> usize {
     score
 }
 
+pub fn myers_unbounded(query: &str, text: &str) -> usize {
+    let hsize = if text.len() % 64 == 0 {
+        text.len() / 64
+    } else {
+        text.len() / 64 + 1
+    };
+    let vsize = if query.len() % 64 == 0 {
+        query.len() / 64
+    } else {
+        query.len() / 64 + 1
+    };
+
+    let mut peq: Vec<u64> = vec![0; 256 * vsize];
+    for (i, p) in query.bytes().enumerate() {
+        peq[(i / 64) * 256 + p as usize] |= 1 << (i % 64);
+    }
+
+    let m = query.len();
+    let mut score = m;
+
+    let mut mhc: Vec<u64> = vec![0; hsize];
+    let mut phc: Vec<u64> = vec![u64::max_value(); hsize];
+    let last: u64 = 1 << (m % 64 - 1);
+
+    for b in 0..vsize {
+        let mut mv: u64 = 0;
+        let mut pv: u64 = u64::max_value();
+        score = m;
+
+        for (i, t) in text.bytes().enumerate() {
+            let mut eq: u64 = peq[b * 256 + t as usize];
+            let pb: u64 = (phc[i / 64] >> (i % 64)) & 1;
+            let mb: u64 = (mhc[i / 64] >> (i % 64)) & 1;
+
+            eq |= mb;
+            let xh: u64 = (((eq & pv).wrapping_add(pv)) ^ pv) | eq;
+            let mut ph: u64 = mv | !(xh | pv);
+            let mut mh: u64 = pv & xh;
+
+            if ph & last != 0 {
+                score += 1;
+            }
+            if mh & last != 0 {
+                score -= 1;
+            }
+
+            if ((ph >> 63) ^ pb) > 0 {
+                phc[i / 64] ^= (1 as u64) << (i % 64);
+            }
+            if ((mh >> 63) ^ mb) > 0 {
+                mhc[i / 64] ^= (1 as u64) << (i % 64);
+            }
+
+            ph = (ph << 1) | pb;
+            mh = (mh << 1) | mb;
+
+            pv = mh | !((eq | mv) | ph);
+            mv = ph & (eq | mv);
+        }
+    }
+    score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,5 +106,14 @@ mod tests {
         let query = String::from("kitten!");
         let text = String::from("sitting");
         assert_eq!(myers_64(&query, &text), 3);
+    }
+
+    #[test]
+    fn big_ascii() {
+        let query =
+            String::from("kitten!kitten!kitten!kitten!kitten!kitten!kitten!kitten!kitten!kitten!");
+        let text =
+            String::from("sittingsittingsittingsittingsittingsittingsittingsittingsittingsitting");
+        assert_eq!(myers_unbounded(&query, &text), 30);
     }
 }
